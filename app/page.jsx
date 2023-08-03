259e66012b58a24e9b686869da3f6783291018cd
msm <myatsumon@ucsy.edu.mm>
2023-08-03 08:40:48 +0630

    simplee todo app with crud function

---
+++ app/page.jsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="text-red-200 text-4xl">Home</h1>
+      <p className="py-10">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Error vitae
+        mollitia voluptatem dicta pariatur velit et dolor minima ex consequatur.
+        Et, suscipit expedita officiis rerum exercitationem, iure atque sapiente
+        libero deserunt quibusdam corporis cupiditate delectus quis nesciunt
+        numquam veniam similique harum tenetur. Hic reprehenderit quae ea
+        molestias repudiandae praesentium ullam.
+      </p>
+      <Link href="/about">About Page</Link>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ app/posts/error.jsx
@@ -0,0 +1,7 @@
+"use client";
+
+const error = () => {
+  return <div>There is a data fetching error...</div>;
+};
+
+export default error;
